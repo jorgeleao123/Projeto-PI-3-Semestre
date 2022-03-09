b5043f7ffa0768091122de6f81639ef2db730a3b
chris-hitoshi <tetihamai@gmail.com>
2022-03-09 17:22:04 -0300

    API de login e cadastro java

---
+++ api.go/src/main/java/api/go/entidades/Usuario.java
@@ -0,0 +1,77 @@
+package api.go.entidades;
+
+public class Usuario {
+    private int idUsuario;
+    private String nomeUsuario;
+    private String loginUsuario;
+    private String senhaUsuario;
+    private Boolean isAdmin;
+    private Boolean isMotorista;
+    private Boolean isAutenticado;
+
+    public Usuario(int idUsuario, String nomeUsuario, String loginUsuario, String senhaUsuario, Boolean isAdmin, Boolean isMotorista) {
+        this.idUsuario = idUsuario;
+        this.nomeUsuario = nomeUsuario;
+        this.loginUsuario = loginUsuario;
+        this.senhaUsuario = senhaUsuario;
+        this.isAdmin = isAdmin;
+        this.isMotorista = isMotorista;
+        this.isAutenticado = false;
+    }
+
+    public Boolean getAutenticado() {
+        return isAutenticado;
+    }
+
+    public void setAutenticado(Boolean autenticado) {
+        isAutenticado = autenticado;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getLoginUsuario() {
+        return loginUsuario;
+    }
+
+    public void setLoginUsuario(String loginUsuario) {
+        this.loginUsuario = loginUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Boolean getMotorista() {
+        return isMotorista;
+    }
+
+    public void setMotorista(Boolean motorista) {
+        isMotorista = motorista;
+    }
+}
