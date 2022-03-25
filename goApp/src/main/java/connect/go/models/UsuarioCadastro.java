f333ea8a5d9a7b57c8c2f536684b12d8acdc4f24
mauricioishikawa <mauricio.ishikawa@alpenet.com.br>
2022-03-25 12:25:22 -0300

    cadastro e login configurados no JPA para banco H2

---
+++ goApp/src/main/java/connect/go/models/UsuarioCadastro.java
@@ -0,0 +1,22 @@
+package connect.go.models;
+
+public class UsuarioCadastro {
+    private Usuario usuario;
+    private Endereco endereco;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
