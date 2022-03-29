bb0d8f005eae55f1da217b5064b66bc11befe724
Christian Hitoshi <tetihamai@gmail.com>
2022-03-29 18:30:57 -0300

    Iniciando parte do User Data

--- frontend/go-app/src/js/App.js
+++ frontend/go-app/src/js/App.js
@@ -1,10 +1,15 @@
-
+import '../css/style.css';
 import Menu from "./Menu.js";
+import UserData from './UserData';
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <hr />
+      <div class="content">
+        <UserData />
+      </div>
     </div>
   );
 }
---
+++ frontend/go-app/src/js/UserData.js
@@ -0,0 +1,18 @@
+import '../css/user-data.css';
+
+function UserData(){
+    return (
+        <div class="user-data">
+            <div class="user-information">
+                <div class="user-image"></div>
+                <div class="user-name">
+                    <h3>Rafaela Kimberlly</h3>
+                    <h6>São Paulo</h6>
+                </div>
+            </div>
+            <hr />
+        </div>
+    );
+}
+
+export default UserData;
