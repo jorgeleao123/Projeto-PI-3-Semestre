7f5af56d2d22c16d8fc269cf1f0d4a1e7902ca53
Christian Hitoshi <tetihamai@gmail.com>
2022-03-29 19:33:16 -0300

    Adicionando fonte chivo na aplicação

--- frontend/go-app/src/js/UserData.js
+++ frontend/go-app/src/js/UserData.js
@@ -2,7 +2,7 @@ import '../css/user-data.css';
 
 function UserData(){
     return (
-        <div class="user-data">
+        <div class="user-data-card">
             <div class="user-information">
                 <div class="user-image"></div>
                 <div class="user-name">
@@ -11,6 +11,16 @@ function UserData(){
                 </div>
             </div>
             <hr />
+            <div class="user-data">
+                <div class="publication">
+                    <span>Publicações</span>
+                    <span>2</span>
+                </div>
+                <div class="search">
+                    <span>Buscas</span>
+                    <span>14</span>
+                </div>
+            </div>
         </div>
     );
 }
