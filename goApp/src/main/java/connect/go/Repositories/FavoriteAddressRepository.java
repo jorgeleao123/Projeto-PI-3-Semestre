fd2ab34b6ac003deac35fc328a02441e7afa159d
Christian Hitoshi <tetihamai@gmail.com>
2022-04-18 21:38:43 -0300

    subindo telas user profile

--- goApp/src/main/java/connect/go/Repositories/FavoriteAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package connect.go.Repositories;
-
-import connect.go.models.FavoriteAddress;
-import connect.go.models.FavoriteAddressId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteAddressRepository extends JpaRepository<FavoriteAddress, FavoriteAddressId> {
-}
