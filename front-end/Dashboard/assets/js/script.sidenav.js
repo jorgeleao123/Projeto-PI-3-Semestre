a0f4690ed4e43bc93d0f752741ffe5edbd61fe73
taisfermino <tais.souza@bandtec.com.br>
2022-05-18 15:09:42 -0300

    Feat: Adicionando Dashboard final

---
+++ front-end/Dashboard/assets/js/script.sidenav.js
@@ -0,0 +1,21 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("maindash").style.marginLeft = "250px";
+}
+
+openNav()==true;
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("maindash").style.marginLeft= "0";
+}
+
+  var navItems = document.querySelectorAll(".mobile-bottom-nav__item");
+navItems.forEach(function(e, i) {
+	e.addEventListener("click", function(e) {
+		navItems.forEach(function(e2, i2) {
+			e2.classList.remove("mobile-bottom-nav__item--active");
+		})
+		this.classList.add("mobile-bottom-nav__item--active");
+	});
+});
